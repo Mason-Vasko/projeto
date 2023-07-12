06022d008f28899ff1b0112b90d9265a3e0ad8c0
Abin <emaildamason@gmail.com>
2023-07-12 09:55:25 -0300

    Modularização
    Removi a tokenização (strtok), agora as strings vão até o espaço ou \n e são palavras individuais

--- Projeto_ICC.c
+++ Projeto_ICC.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef char *string; // Cria artificialmente um tipo de dado string
+typedef char *String; // Cria artificialmente um tipo de dado string
 
 char *readLine() { // Essa função lê a entrada até o usuário pressionar enter,
                    // sem deixar poluição na entrada
@@ -27,6 +27,30 @@ char *readLine() { // Essa função lê a entrada até o usuário pressionar ent
   return string;
 }
 
+
+char *readString() { // Essa função lê a entrada até o usuário pressionar enter,
+                   // sem deixar poluição na entrada
+  char *string = NULL;
+  char currentInput;
+  int index = 0;
+  do {
+    // cada caractere obtido da entrada padrão é direcionado para a inserção
+    // atual (current input), em seguida é alocado espaço adequado na string
+    // para armazenar esse caractere.
+    currentInput = (char)getchar();
+    string = (char *)realloc(string, sizeof(char) * (index + 1));
+    string[index] = currentInput;
+    index++;
+    if (currentInput ==
+        '\r') { // O /r costuma vir antes do /n em sistemas windows.
+      currentInput = (char)getchar();
+    }
+  } while ((currentInput != ' ') && (currentInput != EOF) && (currentInput != '\n'));
+  string[index - 1] = '\0'; // substitui o caracter final (a quebra de página)
+                            // por /0 para oficializar a string
+  return string;
+}
+
 /*void iniciar_dia(FILE *adda, int *estoque, float *caixa){
 
     adda = fopen("dia_anterior.txt","r+");
@@ -42,53 +66,76 @@ char *readLine() { // Essa função lê a entrada até o usuário pressionar ent
 
 } */
 
+void IP(){
+
+printf("\nEssa é a função insereProduto\n");
+
+}
+
+void AE(){
+
+}
+
+void MP(){
+
+}
+
+void VE(){
+
+}
+
+void CE(){
+
+}
+
+void CS(){
+
+}
+
+void FE(){
+  
+}
+
+void programa_mercado(){
+
+String comando;
+int i = 1;
+while(i){
+ 
+  comando = readString();
+  printf("%s",comando);
+
+  if(strcmp(comando, "IP") == 0){
+    IP();
+  }
+  else if(strcmp(comando, "AE") == 0){
+    AE();
+  }
+  else if(strcmp(comando, "MP") == 0){
+    MP();
+  }
+  else if(strcmp(comando, "VE") == 0){
+    VE();
+  }
+  else if(strcmp(comando, "CE") == 0){
+    CE();
+  }
+  else if(strcmp(comando, "CS") == 0){
+    CS();
+  }
+  else if(strcmp(comando, "FE") == 0){
+    FE();
+    i = 0;
+  }
+
+
+}  
+
+
+}
 
 int main(void) {
 
-//FILE *arquivo_do_dia_anterior;
-int espaço_estoque;
-float saldo_do_caixa;
-        string nome = NULL;
-        string quantidade = NULL;
-        string produto = NULL;
-
-
-//iniciar_dia(arquivo_do_dia_anterior, &espaço_estoque, &saldo_do_caixa);
-
-
-
-string comando = readLine();
-string token = strtok(comando, " ");
-    if((strcmp(token, "IP\0") == 0) && token != NULL){
-
-        printf("Inserindo produto. Isso é apenas um protótipo\n");
-        int i = 0;
-        while((token = strtok(NULL, " ")) != NULL){
-            
-            switch (i) {
-            case 0:
-                nome = strdup(token);
-                i++;
-                break;
-            
-            case 1:
-                quantidade = strdup(token);
-                i++;
-                break;
-            case 2:
-                produto = strdup(token);
-                i++;
-                break;
-            }
-        }
-
-    }
-
-    printf("%s\n%s\n%s\n",nome,quantidade,produto);
-    free(nome);
-    free(quantidade);
-    free(produto);
-
-  //fclose(arquivo_do_dia_anterior);
+  programa_mercado();
   return 0;
 }
